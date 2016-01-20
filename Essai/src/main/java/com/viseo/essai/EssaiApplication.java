7582bcb62c222f9f2029c96f969b591f59fbb3a2
otrichot <olivier.trichot@viseo.com>
2016-01-20 15:01:32 +0100

    test and push

---
+++ Essai/src/main/java/com/viseo/essai/EssaiApplication.java
@@ -0,0 +1,12 @@
+package com.viseo.essai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssaiApplication.class, args);
+	}
+}
